225119889d37933c3e5e3ba1d87d0fea68b32ad4
stark <akshaystark3000@gmail.com>
2024-06-26 21:45:57 +0530

    db error resolved

--- utils/database.js
+++ utils/database.js
@@ -1,25 +1,27 @@
-import mongoose from 'mongoose'
 
+import mongoose from 'mongoose';
 
-let isConnected = false;
-export const connectToDB = async()=>{
-    mongoose.set('strictQuery',true);
-    if(isConnected){
-        console.log("DB already connected ");
-        return;
-    }
-    try {
-        await mongoose.connect(process.env.MONGODB_URI,{
-            dbName:"elicit_ai",
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        }
+let isConnected = false; 
 
-        )
-        isConnected= true;
-        console.log("DB connected");
-    } catch (error) {
-        console.log(error);
-        
-    }
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
+
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
